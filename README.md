85a80e43b6aa30384b8980c909312eb921c2e0b0
Poppy <poppy@MacBook-Pro-som-tillhor-Poppy.local>
2020-06-01 19:27:38 +0200

    Basic setup

--- README.md
+++ README.md
@@ -27,3 +27,15 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+## Colors
+```
+primary: #DCE5F0
+secondary: #99ADCC
+accent: #899BB7
+color1: #2D345A
+color2: #6696C1
+color3: #3A48AA
+color4: #A36950
+```
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,24 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#4285F4",
+        secondary: "#424242",
+        accent: "#82B1FF",
+        error: "#ff4444",
+        info: "#33b5e5",
+        success: "#00C851",
+        warning: "#ffbb33",
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+      },
+    },
+  },
 });
